ae72451ed46b8e2be16ca70f768da4aac28949bc
Christoph Zwirello <c.zwirello@thenewmotion.com>
2015-12-01 11:18:07 +0100

    TNM-1509 Locations client.

--- ocpi-endpoints/src/main/scala/com/thenewmotion/ocpi/handshake/HandshakeClient.scala
+++ ocpi-endpoints/src/main/scala/com/thenewmotion/ocpi/handshake/HandshakeClient.scala
@@ -1,48 +1,20 @@
 package com.thenewmotion.ocpi.handshake
 
 import akka.actor.ActorRefFactory
-import akka.util.Timeout
-import com.thenewmotion.ocpi._
+import com.thenewmotion.ocpi.common.OcpiClient
 import com.thenewmotion.ocpi.handshake.Errors._
 import com.thenewmotion.ocpi.msgs.v2_0.CommonTypes.SuccessResp
 import com.thenewmotion.ocpi.msgs.v2_0.Credentials.Creds
 import com.thenewmotion.ocpi.msgs.v2_0.Versions._
 import spray.client.pipelining._
 import spray.http._
-import spray.httpx.unmarshalling._
+import spray.httpx.SprayJsonSupport._
 import scala.concurrent.{ExecutionContext, Future}
-import scala.concurrent.duration._
 import scalaz.Scalaz._
 import scalaz._
 
-class HandshakeClient(implicit refFactory: ActorRefFactory) {
-
+class HandshakeClient(implicit refFactory: ActorRefFactory) extends OcpiClient {
   import com.thenewmotion.ocpi.msgs.v2_0.OcpiJsonProtocol._
-  import spray.httpx.SprayJsonSupport._
-
-  private val logger = Logger(getClass)
-
-  // setup request/response logging
-  val logRequest: HttpRequest => HttpRequest = { r => logger.debug(r.toString); r }
-  val logResponse: HttpResponse => HttpResponse = { r => logger.debug(r.toString); r }
-
-  implicit val timeout = Timeout(10.seconds)
-
-  def request(auth: String)(implicit ec: ExecutionContext) = (
-    addCredentials(GenericHttpCredentials("Token", auth, Map()))
-      ~> logRequest
-      ~> sendReceive
-      ~> logResponse
-    )
-
-  def unmarshalToOption[T](implicit unmarshaller: FromResponseUnmarshaller[T], ec: ExecutionContext):
-  Future[HttpResponse] => Future[Option[T]] = {
-
-    _.map { res =>
-      if (res.status.isFailure) None
-      else Some(unmarshal[T](unmarshaller)(res))
-    }
-  }
 
   def getVersions(uri: Uri, auth: String)(implicit ec: ExecutionContext): Future[HandshakeError \/ VersionsResp] = {
     val pipeline = request(auth) ~> unmarshalToOption[VersionsResp]
@@ -60,6 +32,4 @@ class HandshakeClient(implicit refFactory: ActorRefFactory) {
     val pipeline = request(auth) ~> unmarshalToOption[SuccessResp]
     pipeline(Post(uri, creds)) map { toRight(_)(SendingCredentialsFailed) }
   }
-
 }
-
